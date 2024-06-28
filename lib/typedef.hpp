867c0acb1421b9bd55dc15e953c4d080d1178639
Ryan Lockhart <ryanjeffreylockhart@gmail.com>
2024-06-28 16:07:12 -0400

    some strange changes...

--- lib/typedef.hpp
+++ lib/typedef.hpp
@@ -1,79 +1,89 @@
 #pragma once
 
-namespace Bleakdepth {
-
 #if defined(__x86_64__)
 #define BLEAKDEPTH_64
 #elif defined(__i386__)
 #define BLEAKDEPTH_32
 #endif
 
-	typedef unsigned char u8;
-	typedef signed char i8;
+#define forever for(;;)
 
-	typedef unsigned short u16;
-	typedef signed short i16;
+namespace Bleakdepth {
+	using u8 = unsigned char;
+	using i8 = signed char;
 
-	typedef unsigned int u32;
-	typedef signed int i32;
+	using u16 = unsigned short;
+	using i16 = signed short;
 
-	typedef unsigned long long u64;
-	typedef signed long long i64;
+	using u32 = unsigned int;
+	using i32 = signed int;
 
-	typedef unsigned __int128 u128;
-	typedef signed __int128 i128;
+	using u64 = unsigned long long;
+	using i64 = signed long long;
 
-	typedef _Float16 f16;
-	typedef float f32;
-	typedef double f64;
+	using u128 = unsigned __int128;
+	using i128 = signed __int128;
+
+	using f16 = _Float16;
+	using f32 = float;
+	using f64 = double;
+	using f80 = long double;
+	using f128 = __float128;
 
 #if defined(BLEAKDEPTH_32)
 
-	typedef u32 usize;
-	typedef i32 isize;
+	using usize = u32;
+	using isize = i32;
 
-	typedef u16 uhalf;
-	typedef i16 ihalf;
+	using uhalf = u16;
+	using ihalf = i16;
 
-	typedef u8 uqrtr;
-	typedef i8 iqrtr;
+	using uqrtr = u8;
+	using iqrtr = i8;
 
-	typedef f32 fsize;
-	typedef f16 fhalf;
+	using fsize = f32;
+	using fhalf = f16;
 
 #else
 
-	typedef u64 usize;
-	typedef i64 isize;
+	using usize = u64;
+	using isize = i64;
 
-	typedef u32 uhalf;
-	typedef i32 ihalf;
+	using uhalf = u32;
+	using ihalf = i32;
 
-	typedef u16 uqrtr;
-	typedef i16 iqrtr;
+	using uqrtr = u16;
+	using iqrtr = i16;
 
-	typedef f64 fsize;
-	typedef f32 fhalf;
+	using fsize = f64;
+	using fhalf = f32;
 
 #endif
 
 	template<typename T> using ref = T&;
 	template<typename T> using cref = const T&;
 	template<typename T> using rval = T&&;
+	template<typename T> using crval = const T&&;
 
 	template<typename T> using ptr = T*;
 	template<typename T> using cptr = const T*;
 	template<typename T> using ptrc = T* const;
 	template<typename T> using cptrc = const T* const;
 
-	typedef ptr<void> mem;
-	typedef cptr<void> cmem;
+	using ubyte = u8;
+	using ibyte = i8;
 
-	typedef ptr<char> str;
-	typedef cptr<char> cstr;
+	using mem = ptr<void>;
+	using cmem = cptr<void>;
 
-	typedef ptr<u8> buffer;
-	typedef cptr<u8> cbuffer;
+	using str = ptr<char>;
+	using cstr = cptr<char>;
+
+	using buffer = ptr<ubyte>;
+	using cbuffer = cptr<ubyte>;
+
+	template<typename Return, typename... Args> using fn_ptr = Return(*)(Args...);
+	template<typename T, typename Return, typename... Args> using mfn_ptr = Return(T::*)(Args...);
 
 	template<typename T> constexpr inline T wrap(T value, T min, T max) {
 		if (value < min) {
